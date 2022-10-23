16a4d266b3b144e0959b617a909ed6e0345dda50
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-10-23 16:32:02 +0700

    Update SQLQuery7.sql

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -53,17 +53,31 @@ select * from Asm4_DONGXES where SoChoNgoi > 5;
 --dòng xe thuộc hãng xe “KIA” với mức phí có đơn giá là 20.000 VNĐ/km
 select * from Asm4_NHACUNGCAPS where MaNhaCC in 
 (select MaNhaCC from Asm4_DANGKYDICHVUS where ( Dongxe in (select Dongxe from Asm4_DONGXES where Hangxe like 'Toyota') and MaMP in 
-( select MaMP from Asm4_MUCPHIS where Dongia = 15 ) ) or (Dongxe in (select Dongxe from Asm4_DONGXES where Hangxe like 'KIA' )
- and  MaMP in  ( select MaMP from Asm4_MUCPHIS where Dongia = 20 ) ) );
+( select MaMP from Asm4_MUCPHIS where Dongia = 15 ) ) 
+or (Dongxe in (select Dongxe from Asm4_DONGXES where Hangxe like 'KIA' )
+ and  MaMP in  ( select MaMP from Asm4_MUCPHIS where Dongia = 20 ) ) 
+ 
+ );
 
+ --chữa cách 2 <khuyên ko nên do chậm hơn>
+ select distinct B.* from Asm4_DANGKYDICHVUS A
+ inner join Asm4_NHACUNGCAPS B on A.MaNhaCC = B.MaNhaCC
+inner join Asm4_MUCPHIS C on A.MaMP = C.MaMP 
+ inner join Asm4_DONGXES D on A.Dongxe = D.Dongxe
+where (D.Dongxe like 'Toyota' and C.Dongia = 15) or 
+(D.Dongxe like 'KIA' and C.Dongia = 20);
 
 --Câu 5: Liệt kê thông tin toàn bộ nhà cung cấp được sắp xếp tăng dần theo tên nhà cung cấp và giảm dần theo mã số thuế
+
 select * from Asm4_NHACUNGCAPS order by TenNhaCC asc, MaSoThue desc;
+
 --Câu 6: Đếm số lần đăng ký cung cấp phương tiện tương ứng cho từng nhà cung cấp với
 --yêu cầu chỉ đếm cho những nhà cung cấp thực hiện đăng ký cung cấp có ngày  bắt đầu cung cấp là “20/11/2015”
 
 select MaNhaCC,count(MaDKCC) as SolanCungcap from  Asm4_DANGKYDICHVUS 
-where NgayBatDauCungCap = '2015-11-20' group by MaNhaCC;
+where NgayBatDauCungCap >= '2015-11-20' group by MaNhaCC;
+
+
 
 --Câu 7: Liệt kê tên của toàn bộ các hãng xe có trong cơ sở dữ liệu với yêu cầu mỗi hãng xe chỉ được liệt kê một lần
 
@@ -79,13 +93,36 @@ right join Asm4_DONGXES C on A.Dongxe = C.Dongxe
 right join Asm4_LOAIDICHVUS D on A.MaLoaiCC = D.MaLoaiDV
 right join Asm4_MUCPHIS E on A.MaMP = E.MaMP Order by TenNhaCC asc;
 
+--chữa
+
+select Distinct a.MaDKCC,a.MaNhaCC,B.TenNhaCC,B.DiaChi,
+B.MaSoThue,E.TenLoaiDV, C.DonGia, D.Hangxe, A.NgayBatDauCungCap, A.NgayKetThucCungCap  from  Asm4_DANGKYDICHVUS A
+right join Asm4_NHACUNGCAPS B on A.MaNhaCC = B.MaNhaCC
+left join Asm4_MUCPHIS C on A.MaMP = C.MaMP 
+Left join Asm4_DONGXES D on A.Dongxe = D.Dongxe
+left join Asm4_LOAIDICHVUS E on A.MaLoaiCC = E.MaLoaiDV
+;
+
+
 --Câu 9: Liệt kê thông tin của các nhà cung cấp đã từng đăng ký cung cấp phương tiện
 --thuộc dòng xe “Hiace” hoặc từng đăng ký cung cấp phương tiện thuộc dòng xe “Cerato”
 
 select * from Asm4_NHACUNGCAPS where MaNhaCC in 
 (select MaNhaCC from Asm4_DANGKYDICHVUS where Dongxe like 'Hiace' or Dongxe like 'Cerato') 
 
+--chữa 
+
+
+select * from Asm4_NHACUNGCAPS where MaNhaCC in 
+(select MaNhaCC from Asm4_DANGKYDICHVUS where Dongxe in ('Hiace','Cerato')) 
+
+
 --Câu 10: Liệt kê thông tin của các nhà cung cấp chưa từng thực hiện đăng ký cung cấp phương tiện lần nào cả.
 
 select * from Asm4_NHACUNGCAPS where MaNhaCC in 
-(select MaNhaCC from Asm4_DANGKYDICHVUS where Dongxe like null );
+(select MaNhaCC from Asm4_DANGKYDICHVUS where Dongxe like null ); --sai
+
+--chữa
+
+select * from Asm4_NHACUNGCAPS where MaNhaCC not in
+(select MaNhaCC from Asm4_DANGKYDICHVUS)
